8e366c1b5f3a44dafab8ce14fd3dca9fdfc85d32
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-27 17:00:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function removeColor() {
+  const selectedColor = document.getElementById("colorSelect").value;
+  document.getElementById("colorSelect").remove(document.getElementById("colorSelect").selectedIndex);
+}
