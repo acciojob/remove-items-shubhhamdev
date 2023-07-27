37319cfd2600904bd9926896f266f09a3f173d33
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-07-27 17:06:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// script.js
+
+// Function to remove the selected color option from the dropdown list
 function removeColor() {
   var selectElement = document.getElementById("colorSelect");
   var selectedIndex = selectElement.selectedIndex;
+
   if (selectedIndex !== -1) {
     selectElement.remove(selectedIndex);
   }
 }
+
+// Event listener for the button click
+document.addEventListener("DOMContentLoaded", function () {
+  var removeButton = document.querySelector("input[type='button']");
+  removeButton.addEventListener("click", removeColor);
+});
